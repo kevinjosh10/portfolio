7c4efe6f04730fdc1a805f62b9144d50e48853a2
kevinjosh10 <kingkevinjosh7@gmail.com>
2026-01-14 20:48:29 +0530

    s

--- main1.js
+++ main1.js
@@ -1,22 +1,15 @@
 const btn = document.getElementById("knowBtn");
-const aboutSection = document.getElementById("about");
+const about = document.getElementById("about");
 const sections = document.querySelectorAll(".section");
 
 btn.addEventListener("click", () => {
-  aboutSection.scrollIntoView({ behavior: "smooth" });
-
-  sections.forEach((sec, i) => {
-    setTimeout(() => {
-      sec.classList.add("show");
-    }, i * 200);
-  });
+  about.scrollIntoView({ behavior: "smooth" });
 });
 
-// Reveal on scroll
 window.addEventListener("scroll", () => {
   sections.forEach(sec => {
     const rect = sec.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
+    if (rect.top < window.innerHeight - 120) {
       sec.classList.add("show");
     }
   });
